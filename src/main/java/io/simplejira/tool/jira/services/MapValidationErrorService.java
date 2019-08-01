8be499186ca6dc0a02d34ac96c3338d22f7676dd
enigma420 <enigma3420@gmail.com>
2019-08-01 15:26:56 +0200

    Custom Exceptions

--- src/main/java/io/simplejira/tool/jira/services/MapValidationErrorService.java
+++ src/main/java/io/simplejira/tool/jira/services/MapValidationErrorService.java
@@ -2,12 +2,14 @@ package io.simplejira.tool.jira.services;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class MapValidationErrorService {
 
     public ResponseEntity<?> MapValidationService(BindingResult result){
