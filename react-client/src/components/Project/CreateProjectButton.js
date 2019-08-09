52744660203c690031b369725dae94467c2b05c8
Dominic <enigma3420@gmail.com>
2019-08-09 14:36:04 +0200

    Create AddProject component => controlled form

--- react-client/src/components/Project/CreateProjectButton.js
+++ react-client/src/components/Project/CreateProjectButton.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const CreateProjectButton = () => {
     return (
         <React.Fragment>
-            <Link to="/addProject" className="btn btn-lg btn-info">
-                Create a Project
-            </Link>
-        </React.Fragment>
-    );
+        <Link to="/addProject" className="btn btn-lg btn-info">
+        Create a Project
+    </Link>
+    </React.Fragment>
+);
 };
 
 export default CreateProjectButton;
