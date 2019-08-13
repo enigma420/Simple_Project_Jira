7fc1b60f8099d77e5d42f3e422eae08858c50eb6
enigma420 <enigma3420@gmail.com>
2019-08-13 12:33:48 +0200

    One to Many User Project implementation

--- src/main/java/io/simplejira/tool/jira/services/ProjectService.java
+++ src/main/java/io/simplejira/tool/jira/services/ProjectService.java
@@ -41,14 +41,15 @@ public class ProjectService {
 
     public Project findProjectByIdentifier(String projectId){
 
+        //Only want to return the project if the user looking for it is the owner
+
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
         if(project == null){
             throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
 
         }
-
-
+        
         return project;
     }
 
